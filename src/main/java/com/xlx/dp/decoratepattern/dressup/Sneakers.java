edb6af154e62116cc7aaf34d5069fddcdef688fc
perfectxlx <perfectxlx@gmail.com>
2019-10-24 20:28:58 +0800

    策略模式

---
+++ src/main/java/com/xlx/dp/decoratepattern/dressup/Sneakers.java
@@ -0,0 +1,18 @@
+package com.xlx.dp.decoratepattern.dressup;
+
+import com.xlx.dp.decoratepattern.Finery;
+
+/**
+ * 球鞋
+ *
+ * @author xielx at 2019/10/24 19:24
+ */
+public class Sneakers extends Finery {
+    
+    
+    @Override
+    public void show() {
+        System.out.println("AJ球鞋");
+        super.show();
+    }
+}
