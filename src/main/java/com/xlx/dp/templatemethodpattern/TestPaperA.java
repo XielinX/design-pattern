eff5834c7246f384cb22d1fadc318e4b906bc40c
perfectxlx <perfectxlx@gmail.com>
2019-10-26 17:22:52 +0800

    模板方法模式

---
+++ src/main/java/com/xlx/dp/templatemethodpattern/TestPaperA.java
@@ -0,0 +1,24 @@
+package com.xlx.dp.templatemethodpattern;
+
+/**
+ * 考生A作答的试卷
+ *
+ * @author xielx at 2019/10/26 16:56
+ */
+public class TestPaperA extends MathTestPaper {
+    
+    @Override
+    protected String answer1() {
+        return "2";
+    }
+    
+    @Override
+    protected String answer2() {
+        return "21";
+    }
+    
+    @Override
+    protected String answer3() {
+        return "23";
+    }
+}
