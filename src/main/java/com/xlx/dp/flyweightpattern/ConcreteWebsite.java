8ef6c94beec2cf98bbe431afa9a0f3b1b654a7e0
perfectxlx <perfectxlx@gmail.com>
2019-11-12 16:11:50 +0800

    享元模式

---
+++ src/main/java/com/xlx/dp/flyweightpattern/ConcreteWebsite.java
@@ -0,0 +1,24 @@
+package com.xlx.dp.flyweightpattern;
+
+/**
+ * 网站具体类
+ *
+ * @author xielx at 2019/11/12 15:24
+ */
+public class ConcreteWebsite extends WebSite{
+    
+    /**
+     * 网站分类
+     */
+    private String classfild;
+    
+    
+    public ConcreteWebsite(String classfild){
+        this.classfild = classfild;
+    }
+    
+    @Override
+    protected void use(User user) {
+        System.out.println("网站分类:" + classfild + ",用户:" + user.getName() );
+    }
+}
